f39e97e9d9529e94941af9e279823137a8c74db4
Alex Bui <alex46bui@gmail.com>
2023-08-02 01:09:54 -0700

    Updated CSS styling for profile page and NavBar
    Used Twitter API to render tweets. Static at the moment

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,27 +2,19 @@ import React, {useRef} from "react";
 import {firestore } from "../firebase"
 import {addDoc, collection} from "firebase/firestore"
 import './css/Home.css'
+import { eagerLoadTwitterLibrary, Tweet} from 'react-twitter-widgets'
 
 const Home = () => {
-    const messageRef = useRef();
-    const ref = collection(firestore, "messages");
-
-
-    const handleSubmit = async(e) => {
-        e.preventDefault();
-        console.log(messageRef.current.value);
-        let data = {
-            message : messageRef.current.value
-        }
-        try{
-            addDoc(ref, data)
-        }catch{
-            console.log(e)
-        }
-    }
+    eagerLoadTwitterLibrary();
     return (
         <div className="Home-container">
-            <h1> home </h1>
+            <h1 className = "home"> Home </h1>
+            <Tweet 
+            options = {{align: 'left',width: '1800px'}}
+            tweetId = '1686624230035587072' 
+            />
+            <Tweet options = {{align: 'left'}}tweetId = '1686161726121619458' />
+             
         </div>
     )
 }
